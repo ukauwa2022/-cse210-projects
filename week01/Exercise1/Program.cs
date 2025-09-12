9250a8eac958e519361ffb7d9d0997976f37f396
ukauwa2022 <115455966+ukauwa2022@users.noreply.github.com>
2025-09-12 17:54:51 +0100

    Update Program.cs
    Added command

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask the user for their name.
+        Console.WriteLine("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last} ");
     }
 }
